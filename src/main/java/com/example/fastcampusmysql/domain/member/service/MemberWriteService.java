7bdb3ce4a77e0ae6e06244fdbdd79083f1564f33
yunyoung1819 <yunyoung1819@hanmail.net>
2023-03-21 16:53:28 +0900

    [feature] JDBC Template을 이용한 회원정보 조회 구현 실습

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class MemberWriteService {
 	final private MemberRepository memberRepository;
 
-	public Member create(RegisterMemberCommand command) {
+	public Member register(RegisterMemberCommand command) {
 		/**
 		 * 목표 - 회원정보(이메일, 닉네임, 생년월일)를 등록한다.
 		 *     - 닉네임은 10자를 넘길 수 없다.
